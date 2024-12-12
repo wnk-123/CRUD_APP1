f5153c573674e63e7e780edc00c3eddb69672e6d
wnk-123 <nickitayarmak123@gmail.com>
2024-12-12 17:57:25 +0200

    crud_app_1

---
+++ src/main/java/com/wnk/crud_app1/controllers/PeopleController.java
@@ -0,0 +1,48 @@
+package com.wnk.crud_app1.controllers;
+
+import com.wnk.crud_app1.dao.PersonDAO;
+import com.wnk.crud_app1.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public PeopleController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping()
+    public String index(Model model) {
+        // Получим всех людей из DAO и передадим на отображение в представление
+
+        model.addAttribute("people", personDAO.index());
+        return "people/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        // Получим одного человека по id из DAO и передадим на отображение в представление
+
+        model.addAttribute("person", personDAO.show(id));
+        return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(Model model) {
+        model.addAttribute("person", new Person());
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
+    }
+}
